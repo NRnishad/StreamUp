c662af3168f67c04f9ffdb23448210f3db1efc28
NRnishad <nrnishad106@gmail.com>
2025-08-19 15:22:20 +0530

    add daisyUi and reactRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
    <h1 className="bg-red-500 text-3xl font-bold underline">
       Hello world!
+      <button class="btn btn-primary" data-theme=''>Button</button>
+
     </h1>
   )
 }
