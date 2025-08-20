f438c3ccce5c6052b198ed3577c199c34132904b
NRnishad <nrnishad106@gmail.com>
2025-08-20 15:23:00 +0530

    just setup the routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
+import HomePage from "./pages/HomePage.jsx"
+import ChatPage from "./pages/ChatPage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import SignUpPage from "./pages/SignUpPage.jsx"
+import NotificationsPage from "./pages/NotificationsPage.jsx"
+import OnboardingPage from "./pages/OnboardingPage.jsx"
+import CallPage from "./pages/CallPage.jsx"
+import { Routes, Route, Navigate } from "react-router"
+
+
+
+
 function App() {
   
 
   return (
-   <h1 className="bg-red-500 text-3xl font-bold underline">
-      Hello world!
-      <button class="btn btn-primary" data-theme=''>Button</button>
+    <div className="h-screen " data-theme='night'>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
 
-    </h1>
+      
+    </div>
+   
   )
 }
 
