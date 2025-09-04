aba09a7c61d21b7a665ddeb327cd72f0ca84c0e3
NRnishad <nrnishad106@gmail.com>
2025-09-04 06:35:46 +0530

    completed the onOboarding session

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,20 +15,22 @@ import useAuthUser from "./hooks/useAuthUser.js"
 function App() {
  
       const {isLoading,authUser}=useAuthUser()
-      
+      const isAuthenticated = Boolean(authUser)
+      const isOnboarded = authUser?.isOnboarded
       if(isLoading) return <PageLoader/>
+
   return (
     <div className="h-screen " data-theme='night'>
      
       <Routes>
-        <Route path="/" element={authUser?<HomePage />:<Navigate to="/login"/>} />
-        <Route path="/login" element={!authUser?<LoginPage />:<Navigate to="/"/>} />
-        <Route path="/signup" element={!authUser?<SignUpPage />:<Navigate to="/"/>} />
-        <Route path="/chat" element={authUser?<ChatPage />:<Navigate to="/login"/>} />
-        <Route path="/notifications" element={authUser?<NotificationsPage />:<Navigate to="/login"/>} />
-        <Route path="/onboarding" element={authUser?<OnboardingPage />:<Navigate to="/login"/>} />
-        <Route path="/call" element={authUser?<CallPage />:<Navigate to="/login"/>} />
-        <Route path="*" element={authUser?<Navigate to="/" replace />:<Navigate to="/login"/>} />
+        <Route path="/" element={isAuthenticated&&isOnboarded?(<HomePage />):(<Navigate to={!isAuthenticated ? "/login":"/onboarding"}/>)} />
+        <Route path="/login" element={!isAuthenticated?<LoginPage />:<Navigate to="/"/>} />
+        <Route path="/signup" element={!isAuthenticated?<SignUpPage />:<Navigate to="/"/>} />
+        <Route path="/chat" element={isAuthenticated?<ChatPage />:<Navigate to="/login"/>} />
+        <Route path="/notifications" element={isAuthenticated?<NotificationsPage />:<Navigate to="/login"/>} />
+        <Route path="/onboarding" element={isAuthenticated?(!isOnboarded ?(<OnboardingPage />):(<Navigate to="/"/>)):(<Navigate to="/login"/>)} />
+        <Route path="/call" element={isAuthenticated?<CallPage />:<Navigate to="/login"/>} />
+        <Route path="*" element={isAuthenticated?<Navigate to="/" replace />:<Navigate to="/login"/>} />
       </Routes>
 
       <Toaster/>
