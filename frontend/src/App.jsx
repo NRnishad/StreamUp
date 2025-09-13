beb675d6f9988c8e339fa8a8b82da02d17bf1282
NRnishad <nrnishad106@gmail.com>
2025-09-13 16:52:55 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,34 @@
-import HomePage from "./pages/HomePage.jsx"
-import ChatPage from "./pages/ChatPage.jsx"
-import LoginPage from "./pages/LoginPage.jsx"
-import SignUpPage from "./pages/SignUpPage.jsx"
-import NotificationsPage from "./pages/NotificationsPage.jsx"
-import OnboardingPage from "./pages/OnboardingPage.jsx"
-import CallPage from "./pages/CallPage.jsx"
-import { Routes, Route, Navigate } from "react-router"
-import {Toaster} from 'react-hot-toast'
-import PageLoader from "./components/PageLoader.jsx"
-import useAuthUser from "./hooks/useAuthUser.js"
-import Layout from "./components/Layout.jsx"
-import { useThemeStore } from "./store/useThemeStore.js"
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
- 
-      const {isLoading,authUser}=useAuthUser()
-      const isAuthenticated = Boolean(authUser)
-      const {theme}=useThemeStore()
-      const isOnboarded = authUser?.isOnboarded
-      if(isLoading) return <PageLoader/>
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="h-screen " data-theme={theme}>
-      {/* ROUTES */}
-      <Routes>
-        <Route path="/" element={isAuthenticated&&isOnboarded?(<Layout showSidebar={true}><HomePage /></Layout>):(<Navigate to={!isAuthenticated ? "/login":"/onboarding"}/>)} />
-        <Route path="/login" element={!isAuthenticated?<LoginPage />:<Navigate to={isOnboarded ? "/":"/onboarding"}/>} />
-        <Route path="/signup" element={!isAuthenticated?<SignUpPage />:<Navigate to={isOnboarded ? "/":"/onboarding"}/>} />
-        <Route path="/chat" element={isAuthenticated?<ChatPage />:<Navigate to="/login"/>} />
-        <Route path="/notifications" element={isAuthenticated?<NotificationsPage />:<Navigate to="/login"/>} />
-        <Route path="/onboarding" element={isAuthenticated?(!isOnboarded ?(<OnboardingPage />):(<Navigate to="/"/>)):(<Navigate to="/login"/>)} />
-        <Route path="/call" element={isAuthenticated?<CallPage />:<Navigate to="/login"/>} />
-        <Route path="*" element={isAuthenticated?<Navigate to="/" replace />:<Navigate to="/login"/>} />
-      </Routes>
-
-      <Toaster/>
-    </div>
-   
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
--- frontend/src/store/useThemeStore.js
+++
@@ -1,8 +0,0 @@
-import {create} from "zustand";
-
-export const useThemeStore = create((set) => ({
-    theme:localStorage.getItem('StremUp-theme') ||'coffee',
-    setTheme: (theme) => {
-        localStorage.setItem('StremUp-theme',theme)
-        set({theme})
-}}));
